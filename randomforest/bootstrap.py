38104b23878553d4d2d3aba636aa3dac9ac96b13
Alencar da Costa <alencaradc@gmail.com>
2021-04-06 20:31:02 -0300

    Refactored random forest module

---
+++ randomforest/bootstrap.py
@@ -0,0 +1,14 @@
+import random
+
+def get_bootstrap(x,y):
+  len_x = len(x)
+
+  bootstrap_x = []
+  bootstrap_y = []
+
+  for i in range(len_x):
+    sample_i = random.randint(0, len_x - 1)
+    bootstrap_x.append(x[sample_i])
+    bootstrap_y.append(y[sample_i])
+
+  return (bootstrap_x, bootstrap_y)
